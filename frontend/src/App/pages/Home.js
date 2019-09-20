f536143e3cff9e4d6de794e617d8ddfa5fe3be13
Eric Christensen <hebrew.8.12@gmail.com>
2019-09-20 16:41:48 -0700

    refactored pages/Home, qol fixes

--- frontend/src/App/pages/Home.js
+++ frontend/src/App/pages/Home.js
@@ -1,144 +1,134 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 // import { Link } from 'react-router-dom';
 import '../App.css';
 import axios from "axios";
 import { Container, Row, Col, Navbar, Form } from 'react-bootstrap';
 
-class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.handlerDataPost = this.handlerDataPost.bind(this)
-    this.servicesGetHandler = this.servicesGetHandler.bind(this)
-    this.radioHandler = this.radioHandler.bind(this)
-    this.state = {
-      serviceOne: 0,
-      serviceTwo: 0,
-      serviceThree: 0,
-      form1: '',
-      form2: '',
-      form3: '',
-      form4: '',
-    }
-  }
-  setStateAsync(state) {
-    return new Promise((resolve) => {
-      this.setState(state, resolve)
-    });
-  }
-  async servicesGetHandler() {
+const Home = () => {
+  return (
+    <div className="App">
+      <HomeNavbar></HomeNavbar>
+      <ServiceForm></ServiceForm>
+    </div>
+  )
+}
+
+const HomeNavbar = () => {
+  return (
+    <div className="App">
+      <Navbar variant="light" className="topBar">
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Navbar>
+    </div>
+  )
+}
+
+const ServiceForm = () => {
+  const [serviceOne, setServiceOne] = useState([])
+  const [serviceTwo, setServiceTwo] = useState([])
+  const [serviceThree, setServiceThree] = useState([])
+  const [radioCheck, setRadioCheck] = useState('')
+  const [statusMessage, setStatusMessage] = useState()
+
+  const getServices = async () => {
     try {
       const res = await axios.get('/services')
-      await this.setStateAsync(() => {
-        return {
-          serviceOne: res.data[0].serviceSeats,
-          serviceTwo: res.data[1].serviceSeats,
-          serviceThree: res.data[2].serviceSeats
-        }
-      })
+      setServiceOne(res.data[0])
+      setServiceTwo(res.data[1])
+      setServiceThree(res.data[2])
     } catch (e) {
-
+      console.log(e.response.status)
     }
   }
-  async handlerDataPost(e) {
-    e.preventDefault();
-    // e.target.elements.form1.value = ''
-    let form2 = e.target.elements.form2.value
-    let form3 = e.target.elements.form3.value
-    let form4 = e.target.elements.form4.value
+
+  const radioHandler = (e) => {
+    setRadioCheck(e.target.value)
+  }
+
+  const formHandler = async (e) => {
+    e.preventDefault()
+    let formObj = {}
+    for (let i = 3; i < (e.target.elements.length - 1); i++) {
+      formObj[i] = e.target.elements[i].value
+      e.target.elements[i].value = ''
+    }
+    console.log(formObj)
+    setRadioCheck('')
     try {
-      await this.setStateAsync(() => { return { form2: form2, form3: form3, form4: form4, } })
       const res = await axios.post('/users',
-        { 'service': this.state.form1, 'seats': this.state.form2, 'name': this.state.form3, 'email': this.state.form4 })
-      console.log("weha", res.data)
+        { 'service': radioCheck, 'seats': formObj[3], 'name': formObj[4], 'email': formObj[5] })
+      setStatusMessage('Submitted Successfully!')
     } catch (e) {
       console.log('Status:', e.response.status)
       if (e.response.status === 400) {
         console.log("400 ERROR")
+        setStatusMessage('Invalid Form Input')
       } else if (e.response.status === 422) {
+        setStatusMessage('This email has already been used')
         console.log("422 ERROR")
       } else {
         console.log("You goofed somehow")
       }
     }
   }
-  radioHandler(e) {
-    let form1 = e.target.value
-    this.setState(() => {
-      return {
-        form1: form1
-      }
-    })
-  }
-  render() {
-    const style1 = 'serviceIcon'
-    const style2 = 'serviceIconClicked'
-    return (
-      <div className="App">
-        <Navbar variant="light" className="topBar">
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        </Navbar>
-        <Container className="topContainer">
-          <Row>
-            <Col></Col>
-            <Col xs={8}></Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col xs={8}></Col>
-            <Col></Col>
-          </Row>
+
+  useEffect(() => {
+    getServices()
+    setInterval(() => getServices(), 3000)
+  }, [])
+
+  return (
+    <div id="form" className="App">
+      <Container>
+        <Form onSubmit={formHandler}>
           <Row>
             <Col lg={2}></Col>
             <Col>
               <div>
                 <p>8:00am</p>
-                <div className={this.state.form1 === 'First Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
-                <Form.Check type="radio" value="First Service" onChange={this.radioHandler} checked={this.state.form1 === 'First Service'} />
+                <div className={radioCheck === 'First Service' ? 'serviceIconClicked' : 'serviceIcon'}>
+                  <ion-icon name="cloudy"></ion-icon>
+                </div>
+                <Form.Check type="radio" value="First Service" onChange={radioHandler} checked={radioCheck === 'First Service'} />
               </div>
-              <p>{this.state.serviceOne}</p>
+              <p key={serviceOne._id}>{serviceOne.serviceSeats}</p>
             </Col>
             <Col>
               <div>
                 <p>9:45am</p>
-                <div className={this.state.form1 === 'Second Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
-                <Form.Check type="radio" value="Second Service" onChange={this.radioHandler} checked={this.state.form1 === 'Second Service'} />
+                <div className={radioCheck === 'Second Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
+                <Form.Check type="radio" value="Second Service" onChange={radioHandler} checked={radioCheck === 'Second Service'} />
               </div>
-              <p>{this.state.serviceTwo}</p>
+              <p key={serviceTwo._id}>{serviceTwo.serviceSeats}</p>
             </Col>
             <Col>
               <div>
                 <p>11:30am</p>
-                <div className={this.state.form1 === 'Third Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
-                <Form.Check type="radio" value="Third Service" onChange={this.radioHandler} checked={this.state.form1 === 'Third Service'} />
+                <div className={radioCheck === 'Third Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
+                <Form.Check type="radio" value="Third Service" onChange={radioHandler} checked={radioCheck === 'Third Service'} />
               </div>
-              <p>{this.state.serviceThree}</p>
+              <p key={serviceTwo._id}>{serviceTwo.serviceSeats}</p>
             </Col>
             <Col lg={2}></Col>
           </Row>
           <Row>
             <Col></Col>
             <Col xs={8} lg={6}>
-              <Form onSubmit={this.handlerDataPost}>
-                <Form.Group>
-                  <Form.Control type="number" min="1" max="10" name="form2" placeholder="How many seats 1-10:"></Form.Control>
-                  <Form.Control type="text" name="form3" placeholder="Enter name:"></Form.Control>
-                  <Form.Control type="email" name="form4" placeholder="Enter email:"></Form.Control>
-                  {/*<Form.Control type="text" name="form4" placeholder="Place Input:"></Form.Control>*/}
-                  <button className="myButton">Submit</button>
-                </Form.Group>
-              </Form>
+              <p className={statusMessage === 'Submitted Successfully!' ? 'successStatus' : 'errorStatus'}>{statusMessage}</p>
+              <Form.Group>
+                <Form.Control type="number" min="1" max="10" name="form2" placeholder="How many seats 1-10:"></Form.Control>
+                <Form.Control type="text" name="form3" placeholder="Enter name:"></Form.Control>
+                <Form.Control type="email" name="form4" placeholder="Enter email:"></Form.Control>
+                <button className="myButton">Submit</button>
+              </Form.Group>
             </Col>
             <Col></Col>
           </Row>
-        </Container>
-      </div >
-    )
-  }
-  componentDidMount() {
-    this.servicesGetHandler()
-  }
+        </Form>
+      </Container>
+    </div>
+  )
 }
 
 export default Home;
--- frontend/src/App/pages/test.js
+++ frontend/src/App/pages/test.js
@@ -1,90 +1,138 @@
-class Admin extends Component {
+class Home extends Component {
     constructor(props) {
         super(props)
-        this.handlerLoginState = this.handlerLoginState.bind(this)
+        this.handlerDataPost = this.handlerDataPost.bind(this)
+        this.servicesGetHandler = this.servicesGetHandler.bind(this)
+        this.radioHandler = this.radioHandler.bind(this)
         this.state = {
-            loggedIn: false
-        }
-    }
-    handlerLoginState(option) {
-        setTimeout(() => this.setState(() => {
-            return {
-                loggedIn: option
-            }
-        }), 1500)
-    }
-    render() {
-        return (
-            <div className="App">
-                {(!this.state.loggedIn) && <Login
-                    handlerLoginState={this.handlerLoginState}
-                />}
-                {(this.state.loggedIn) && <LoggedIn></LoggedIn>}
-            </div>
-        )
-    }
-}
-class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.handlerLogin = this.handlerLogin.bind(this)
-        this.state = {
-            status: '',
+            serviceOne: 0,
+            serviceTwo: 0,
+            serviceThree: 0,
             form1: '',
             form2: '',
-            form3: ''
+            form3: '',
+            form4: '',
         }
     }
-    async handlerLogin(e) {
+    setStateAsync(state) {
+        return new Promise((resolve) => {
+            this.setState(state, resolve)
+        });
+    }
+    async servicesGetHandler() {
+        try {
+            const res = await axios.get('/services')
+            await this.setStateAsync(() => {
+                return {
+                    serviceOne: res.data[0].serviceSeats,
+                    serviceTwo: res.data[1].serviceSeats,
+                    serviceThree: res.data[2].serviceSeats
+                }
+            })
+        } catch (e) {
+
+        }
+    }
+    async handlerDataPost(e) {
         e.preventDefault();
-        let form1 = e.target.elements.form1.value
+        // e.target.elements.form1.value = ''
         let form2 = e.target.elements.form2.value
         let form3 = e.target.elements.form3.value
+        let form4 = e.target.elements.form4.value
+        try {
+            await this.setStateAsync(() => { return { form2: form2, form3: form3, form4: form4, } })
+            const res = await axios.post('/users',
+                { 'service': this.state.form1, 'seats': this.state.form2, 'name': this.state.form3, 'email': this.state.form4 })
+            console.log("weha", res.data)
+        } catch (e) {
+            console.log('Status:', e.response.status)
+            if (e.response.status === 400) {
+                console.log("400 ERROR")
+            } else if (e.response.status === 422) {
+                console.log("422 ERROR")
+            } else {
+                console.log("You goofed somehow")
+            }
+        }
+    }
+    radioHandler(e) {
+        let form1 = e.target.value
         this.setState(() => {
             return {
-                form1: form1,
-                form2: form2,
-                form3: form3
-            }
-        }, async () => {
-            try {
-                await axios.post('/users/login', {
-                    username: this.state.form1,
-                    password: this.state.form2,
-                    secert: this.state.form3
-                })
-                this.setState(() => { return { status: "Loggin successful!" } })
-                this.props.handlerLoginState(true)
-            } catch (e) {
-                this.setState(() => { return { status: "Loggin invalid!" } })
-                this.props.handlerLoginState(false)
+                form1: form1
             }
         })
     }
     render() {
+        const style1 = 'serviceIcon'
+        const style2 = 'serviceIconClicked'
         return (
             <div className="App">
-                <Row>
-                    <Col></Col>
-                    <Col xs={10}><h1 className="header1">Admin Login</h1></Col>
-                    <Col></Col>
-                </Row>
-                <Row>
-                    <Col xs={2} md={4} lg={4} xl={5}></Col>
-                    <Col xs={8} md={4} lg={4} xl={2}>
-                        {(this.state.status) && <p>{this.state.status}</p>}
-                        <Form onSubmit={this.handlerLogin}>
-                            <Form.Group>
-                                <Form.Control type="text" name="form1" placeholder="Username:"></Form.Control>
-                                <Form.Control type="password" name="form2" placeholder="Password:"></Form.Control>
-                                <Form.Control type="password" name="form3" placeholder="Secret:"></Form.Control>
-                                <button className="myButton">Submit</button>
-                            </Form.Group>
-                        </Form>
-                    </Col>
-                    <Col xs={2} md={4} lg={4} xl={5}></Col>
-                </Row>
-            </div>
+                <Navbar variant="light" className="topBar">
+                    <Navbar.Brand href="#home"></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                </Navbar>
+                <Container className="topContainer">
+                    <Row>
+                        <Col></Col>
+                        <Col xs={8}></Col>
+                        <Col></Col>
+                    </Row>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={8}></Col>
+                        <Col></Col>
+                    </Row>
+                    <Row>
+                        <Col lg={2}></Col>
+                        <Col>
+                            <div>
+                                <p>8:00am</p>
+                                <div className={this.state.form1 === 'First Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
+                                <Form.Check type="radio" value="First Service" onChange={this.radioHandler} checked={this.state.form1 === 'First Service'} />
+                            </div>
+                            <p>{this.state.serviceOne}</p>
+                        </Col>
+                        <Col>
+                            <div>
+                                <p>9:45am</p>
+                                <div className={this.state.form1 === 'Second Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
+                                <Form.Check type="radio" value="Second Service" onChange={this.radioHandler} checked={this.state.form1 === 'Second Service'} />
+                            </div>
+                            <p>{this.state.serviceTwo}</p>
+                        </Col>
+                        <Col>
+                            <div>
+                                <p>11:30am</p>
+                                <div className={this.state.form1 === 'Third Service' ? 'serviceIconClicked' : 'serviceIcon'}><ion-icon name="cloudy"></ion-icon></div>
+                                <Form.Check type="radio" value="Third Service" onChange={this.radioHandler} checked={this.state.form1 === 'Third Service'} />
+                            </div>
+                            <p>{this.state.serviceThree}</p>
+                        </Col>
+                        <Col lg={2}></Col>
+                    </Row>
+                    <Row>
+                        <Col></Col>
+                        <Col xs={8} lg={6}>
+                            <Form onSubmit={this.handlerDataPost}>
+                                <Form.Group>
+                                    <Form.Control type="number" min="1" max="10" name="form2" placeholder="How many seats 1-10:"></Form.Control>
+                                    <Form.Control type="text" name="form3" placeholder="Enter name:"></Form.Control>
+                                    <Form.Control type="email" name="form4" placeholder="Enter email:"></Form.Control>
+                                    {/*<Form.Control type="text" name="form4" placeholder="Place Input:"></Form.Control>*/}
+                                    <button className="myButton">Submit</button>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </div >
         )
     }
+    componentDidMount() {
+        this.servicesGetHandler()
+    }
 }
+
+export default Home;
